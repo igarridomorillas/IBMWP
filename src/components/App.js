8b8a742d7727c4a7ffc15e8a8978830b5ba873b1
Yanira <yanirafr@gmail.com>
2020-11-10 09:27:14 +0100

    add React template

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../stylesheet/App.scss";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <>Hola mundo</>;
+  }
+}
+
+export default App;
