1188f86d9b8b590bea8769294bee8161f64451b0
Yanira <yanirafr@gmail.com>
2020-12-01 20:45:20 +0100

    add fetch function

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import "../stylesheet/App.scss";
-// import CardPage from "./Cards/CardPage";
 import Header from "./Header/Header";
 import Form from "./Form/Form";
+import CardPage from "./Cards/CardPage";
+import { sendData } from "../services/fetch";
 
-const App = (props) => {
+const App = () => {
+  useEffect(() => {
+    sendData(1, 4).then((result) => {
+      console.log(result);
+    });
+  });
   return (
     <>
       <Header />
       <main className="main">
-        {/* <CardPage /> */}
-        <Form />
+        <CardPage />
+        {/* <Form /> */}
       </main>
     </>
   );
---
+++ src/services/fetch.js
@@ -0,0 +1,9 @@
+const sendData = (question, answer) => {
+  return fetch(`http://localhost:4443/?qu=${question}&ans=${answer}`)
+    .then((response) => response.json())
+    .then((result) => {
+      return result;
+    });
+};
+
+export { sendData };
