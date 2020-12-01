736e110e4721a35b13fef8cd829bf527afdb7a53
Yanira <yanirafr@gmail.com>
2020-12-01 23:39:30 +0100

    add map to CardPage

--- src/components/Cards/CardPage.js
+++ src/components/Cards/CardPage.js
@@ -1,29 +1,19 @@
 const { default: Card } = require("./Card");
 
 const CardPage = (props) => {
-  let card = "";
-  for (let index in props.cardData.cards) {
-    const element = props.cardData.cards[index];
-    card = <Card cardDetail={element} />;
-  }
+  const card = Object.keys(props.cardData.cards).map((key) => {
+    return (
+      <Card
+        cardDetail={props.cardData.cards[key]}
+        sendClick={props.sendClick}
+      />
+    );
+  });
 
   return (
     <section className="card-page">
       <h2 className="card-page__title">{props.cardData.option}</h2>
-      <ul className="card-page__cards">
-        {/* {cardData} */}
-        {card}
-        {/* <Card
-          description="Con concha"
-          cardData={props.cardData.cards[1]}
-          sendClick={props.sendClick}
-        />
-        <Card
-          description="Sin concha"
-          cardData={props.cardData.cards[2]}
-          sendClick={props.sendClick}
-        /> */}
-      </ul>
+      <ul className="card-page__cards">{card}</ul>
     </section>
   );
 };
